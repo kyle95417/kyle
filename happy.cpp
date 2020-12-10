9f553b80ba32149b99a232d8114f2d7e13ee442a
kyle95417 <kk95417@gmail.com>
2020-12-10 19:56:58 +0800

    first commit

---
+++ happy.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+
+int main(){
+    cout << "hello world";
+    return 0;
+}
